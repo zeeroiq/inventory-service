e4c4e31df8bc3467168e8a4d9b84bbabf8c956cb
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-15 01:17:25 +0530

    mapper and helpers are added. closes #6

--- src/main/java/com/shri/inventory/mappers/BeerInventoryMapper.java
+++ src/main/java/com/shri/inventory/mappers/BeerInventoryMapper.java
@@ -1,4 +1,15 @@
+/*
+ *  Created by zeeroiq on 9/15/20, 12:10 AM.
+ */
+
 package com.shri.inventory.mappers;
 
-public class BeerInventoryMapper {
+import com.shri.inventory.domain.BeerInventory;
+import com.shri.inventory.model.BeerInventoryDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = DateMapper.class)
+public interface BeerInventoryMapper {
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDto);
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
 }
