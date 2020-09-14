e3844e933da3c1092cd529cfef5877bfa973585b
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-15 01:16:17 +0530

    Controller for BeerInventory is added. closes #5

--- src/main/java/com/shri/inventory/controller/BeerInventoryController.java
+++ src/main/java/com/shri/inventory/controller/BeerInventoryController.java
@@ -1,4 +1,35 @@
+/*
+ *  Created by zeeroiq on 9/15/20, 12:49 AM.
+ */
+
 package com.shri.inventory.controller;
 
+import com.shri.inventory.mappers.BeerInventoryMapper;
+import com.shri.inventory.model.BeerInventoryDto;
+import com.shri.inventory.repository.BeerInventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/")
 public class BeerInventoryController {
+
+    private final BeerInventoryRepository inventoryRepository;
+    private final BeerInventoryMapper inventoryMapper;
+
+    @GetMapping("beer/{beerId}/inventory")
+    public List<BeerInventoryDto> listBeerById(@PathVariable("beerId") UUID beerId) {
+        return inventoryRepository.findAllByBeerId(beerId)
+                .stream()
+                .map(inventoryMapper::beerInventoryToBeerInventoryDto)
+                .collect(Collectors.toList());
+    }
 }
