cf0d594ab3dbd89d7c6c2b5fd75fe93ec384e31a
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-15 01:12:27 +0530

    Initial implementation of the domain and the model class has been done. closes #3

--- src/main/java/com/shri/inventory/domain/BaseEntity.java
+++ src/main/java/com/shri/inventory/domain/BaseEntity.java
@@ -1,4 +1,43 @@
+/*
+ *  Created by zeeroiq on 9/15/20, 12:38 AM.
+ */
+
 package com.shri.inventory.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
 public class BaseEntity {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    @Column(length = 40, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+    @Version
+    private Long version;
+    @CreationTimestamp
+    private Timestamp createOn;
+    @UpdateTimestamp
+    private Timestamp modifiedOn;
+
+    public boolean isNew() {
+        return this.id == null;
+    }
 }
--- src/main/java/com/shri/inventory/domain/BeerInventory.java
+++ src/main/java/com/shri/inventory/domain/BeerInventory.java
@@ -1,4 +1,29 @@
+/*
+ *  Created by zeeroiq on 9/15/20, 12:13 AM.
+ */
+
 package com.shri.inventory.domain;
 
-public class BeerInventory {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity
+public class BeerInventory extends BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = -3733555557522669818L;
+
+    private UUID beerId;
+    private String upc;
+    private Integer quantityOnHand = 0;
 }
