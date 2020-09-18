aebf5af43ba79260d20f984c2b2754514f769e14
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-19 00:40:48 +0530

    closes #16

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,9 @@
-DROP DATABASE IF EXISTS inventoryservice;
-DROP USER IF EXISTS `inventory_service`@`%`;
-CREATE DATABASE IF NOT EXISTS inventoryservice CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS `inventory_service`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
-    CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `inventoryservice`.* TO `inventory_service`@`%`;
-FLUSH PRIVILEGES;
+/*
+ * Created by zeeroiq on 9/19/20, 12:35 AM
+ */
+
+INSERT INTO inventoryservice.beer_inventory (id,create_on,modified_on,version,beer_id,quantity_on_hand,upc) VALUES
+('ff39fdd5-63a1-42f7-bef4-de19e562759a','2020-09-16 20:03:57.851000000','2020-09-16 20:03:57.851000000',0,NULL,10,'0631234200032')
+                                                                                                                 ,('b11cf593-6c34-4547-b4de-fb4c11da01e9','2020-09-16 20:03:57.864000000','2020-09-16 20:03:57.864000000',0,NULL,20,'0631234300013')
+                                                                                                                 ,('388eb4f9-be99-4251-9ace-2b6c5fa921cd','2020-09-16 20:03:57.872000000','2020-09-16 20:03:57.872000000',0,NULL,30,'0083783375214')
+;
---
+++ src/main/resources/db.sql
@@ -0,0 +1,7 @@
+DROP DATABASE IF EXISTS inventoryservice;
+DROP USER IF EXISTS `inventory_service`@`%`;
+CREATE DATABASE IF NOT EXISTS inventoryservice CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER IF NOT EXISTS `inventory_service`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
+    CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `inventoryservice`.* TO `inventory_service`@`%`;
+FLUSH PRIVILEGES;
