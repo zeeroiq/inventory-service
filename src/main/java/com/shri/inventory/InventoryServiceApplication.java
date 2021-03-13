5c4987a28408e831254db716438761eeb6cad264
zeero1q <shrikantvishwakrma74@gmail.com>
2021-03-14 03:19:12 +0530

    Docker-Host: configuration added in beer-service, compose file. to resolve the issue to run application in containerized environment. Docker image to forward TCP and UDP traffic to the docker host.
    This container will determine docker host address in the following order
    
    Use ip from environment variable DOCKER_HOST if set
    This allows you to use this image to forward traffic to arbitrary destinations, not only the docker host.
    Try to resolve host.docker.internal (getent ahostsv4 host.docker.internal)
    Defaults to default gateway (ip -4 route show default)
    By default all ports (0:65535) are forwarded to docker host you can override ports by setting environment variable PORTS to a comma separated list of ports and/or port ranges e.g 443,80,8000:9000
    
    warning On Linux systems
    You have to bind your host applications to 0.0.0.0 or bridge network gateway in addition to 127.0.0.1.
    
    Use following docker command to get the bridge network gateway IP address
    
    docker network inspect bridge --format='{{( index .IPAM.Config 0).Gateway}}'
    
    closes #39

--- src/main/java/com/shri/inventory/InventoryServiceApplication.java
+++ src/main/java/com/shri/inventory/InventoryServiceApplication.java
@@ -3,8 +3,10 @@ package com.shri.inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.Serializable;
+
 @SpringBootApplication
-public class InventoryServiceApplication {
+public class InventoryServiceApplication implements Serializable {
 
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
